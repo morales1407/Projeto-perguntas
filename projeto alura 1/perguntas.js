04ae2efe83dd68789e7ceead086ba808e295dcfa
morales1407 <00001085605644sp@al.educacao.sp.gov.br>
2024-11-13 07:45:06 -0300

    Add files via upload

---
+++ projeto alura 1/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'basquete',
+    'Quem é o goat do basquete?',
+    'O goat do basquete é lameloball'
+)
+
+criaCartao(
+    'basquete',
+    'Qual time tem mais titulos da nba?',
+    'Bostoon Celtics'
+)
+
+criaCartao(
+    'basquete',
+    'Quem é o jogador com mais pontos na historia da liga?',
+    'Lamelo ball'
+)
+
+criaCartao(
+    'basquete',
+    'Qual o melhor time da liga?',
+    'Charlot Hornets, porque nele esta lameloball'
+)
